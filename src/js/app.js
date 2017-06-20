fcb6e355306f8b98f5b3a4753a1e285e88df3fc4
JasonSi <pujiaxun@gmail.com>
2017-06-20 16:25:47 +0800

    Implemented LL1 analysing

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,13 @@
 (function(global, undefined) {
+    // LL1 分析过程
     let ll1parser = new LL1Parser();
-    ll1parser.parse('233')
+    try {
+        let ll1Results = ll1parser.parse('m+m*m#')
+    } catch (e) {
+        console.log(e);
+    }
+
+    // 算符优先分析
 
 
-    
 })(typeof window !== 'undefined' ? window : this);
--- src/js/ll1Parser.js
+++ src/js/ll1Parser.js
@@ -1,9 +1,75 @@
 class LL1Parser {
-    constructor(predictionTable) {
-        this.predictionTable = predictionTable;
+    constructor() {
+        this.predictionTable = [
+            ['m', 'S', 'AT'],
+            ['(', 'S', 'AT'],
+            ['+', 'T', '+AT'],
+            [')', 'T', '$'],
+            ['#', 'T', '$'],
+            ['m', 'A', 'BU'],
+            ['(', 'A', 'BU'],
+            ['+', 'U', '$'],
+            ['*', 'U', '*BU'],
+            [')', 'U', '$'],
+            ['#', 'U', '$'],
+            ['m', 'B', 'm'],
+            ['(', 'B', '(S)']
+        ];
     }
 
     parse(sentence) {
-        console.log(sentence);
+        if (!/#$/.test(sentence)) {
+            throw "Error: A sentence should be end with the #.";
+        }
+
+        this.logs = [];
+        this.string = sentence.split('');
+        this.stack = ['#', 'S'];
+        while (this.stack.length > 1) {
+            let last = this.stack.slice(-1).pop();
+            let first = this.string[0];
+            if (last === first) {
+                this.log(`${first} 匹配`);
+                this.stack.pop();
+                this.string.shift();
+            } else {
+                try {
+                    let production = this.readProduction(first, last);
+                    this.log(`${last} -> ${production}`);
+
+                    this.stack.pop();
+                    if (production !== '$') {
+                        production.split('').reverse().forEach((p) => {
+                            this.stack.push(p);
+                        });
+                    }
+                } catch (e) {
+                    this.log(e);
+                    break;
+                }
+
+            }
+            console.log(this);
+        }
+        return this.logs;
+    }
+
+    readProduction(input, nonTernimal) {
+        let prod = this.predictionTable.find((ele) => {
+            return ele[0] === input && ele[1] === nonTernimal;
+        });
+        if (prod) {
+            return prod[2];
+        } else {
+            throw "SyntaxError: Unexpected symbol or it is not a sentence.";
+        }
+    }
+
+    log(rule) {
+        this.logs.push({
+            stack: this.stack.join(''),
+            string: this.string.join(''),
+            rule,
+        });
     }
 }
